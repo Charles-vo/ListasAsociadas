a9f4c66f29b41d7b13432864e57c79b49c595d13
Charles-vo <127904167+Charles-vo@users.noreply.github.com>
2023-04-28 14:42:24 -0600

    Initial commit

---
+++ src/com/cvargas/EntradaTelefono.java
@@ -0,0 +1,12 @@
+package com.cvargas;
+
+// Esta clase encapsula un nombre y un número telefónico.
+
+public class EntradaTelefono {
+    String nombre;
+    String numero;
+    EntradaTelefono(String n, String num) {
+        nombre = n;
+        numero = num;
+    }
+}
